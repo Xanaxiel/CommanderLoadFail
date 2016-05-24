ef78c8899017139ea7516a274c8d283d843ff71e
TehSeph <kazuhiroichi@gmail.com>
2016-05-23 21:23:14 -0500

    news -> steam news

--- src/admin/steam.js
+++ src/admin/steam.js
@@ -8,14 +8,15 @@ const request  = require("request");
 
 const Command = require(__dirname + "/../command.js");
 
-// -- News Command ---------------------------------------------------------------------------------------------------
+// -- Steam Command ---------------------------------------------------------------------------------------------------
 
-class News extends Command {
+class Steam extends Command {
 
 	constructor() {
-		super("News");
-		this.description = "Enable/Disable News updates feed. (Leave empty to toggle.)";
+		super("Steam");
+		this.description = "Enable/Disable Steam news feed. (Leave empty to toggle.)";
 		this.usage       = "<enable/disable>";
+		this.queue       = [];
 		setInterval(this.poll.bind(this), 300000); // poll once per 5 minutes
 	}
 
@@ -28,8 +29,8 @@ class News extends Command {
         else if (params[0] === "enable") toggle = true;
 		else if (params[0] === "disable") toggle = false;
 
-		console.log(`[News] News updates set to "${toggle}".`)
-		global.bot.client.sendMessage(message, `News updates set to "${toggle}".`);
+		console.log(`[Steam] Steam news set to "${toggle}".`)
+		global.bot.client.sendMessage(message, `Steam news set to "${toggle}".`);
 		return this.poll();
 
 	}
@@ -43,43 +44,36 @@ class News extends Command {
 		if (this.queue.length) this.sendQueue();
 
 		// check to make sure news is enabled
-		if (!global.bot.config.newsEnabled) return;
+		if (!global.bot.config.steamEnabled) return;
 
 		// check for news updates and add them to the next queue
 		request({
 			"method" : "GET",
-			"url" : "https://forum.treeofsavior.com/c/news.json",
+			"url" : "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=372000&count=10&format=json",
 			"json" : true
 		}, (error, response, body) => {
 			if (error) console.log("[News]", error);
 			else if (body) {
-				this.createQueue(body.topic_list.topics);
+				for (let item of body.appnews.newsitems) {
+					if (item.gid === global.bot.config.steamLatest) break; // only queue new news
+					this.queue.push(`**${item.title}**:\n${item.url}`);
+				}
+				global.bot.config.steamLatest = body.appnews.newsitems[0].gid; // store the latest id
 			}
 		});
 
 	}
 
 	sendQueue() {
-		console.log(`[Github] Sending ${this.queue.length} new messages.`);
+		console.log(`[Steam] Sending ${this.queue.length} new messages.`);
 		while (this.queue.length) {
 			let message = this.queue.shift();
 			global.bot.client.sendMessage(global.bot.config.updateChannel, message);
 		}
 	}
 
-	createQueue(topics) {
-		for (let topic of topics) {
-			if (topic.id === global.bot.config.newsLatest) break; // only queue new news
-			let url = `https://forum.treeofsavior.com/t/${topic.slug}/${topic.id}`;
-			let content = `**${topic.title}**:`;
-			content += `\n${url}`;
-			this.queue.push(content);
-		}
-		global.bot.config.newsLatest = topics[0].id; // store the latest id
-	}
-
 }
 
 // -- Export Variables -------------------------------------------------------------------------------------------------
 
-module.exports = News;
+module.exports = Steam;
